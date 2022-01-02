e836deb5c881980846a7dd0ab258e93101d09ae3
Ryuuz02 <ryuuz02@outlook.com>
2022-01-02 15:19:23 -0600

    More tutorial stuff

--- Quickstart/QuickstartTutorial.py
+++ Quickstart/QuickstartTutorial.py
@@ -91,3 +91,66 @@ Or you can print it out by itself, this will not change the array you are using,
 print(10*np.sin(test_array1))
 print(test_array1 < 30)
 """
+
+"""
+Most operators work as expected, however if you want o find the matrix product of two arrays, you can use @
+test_array1 = np.array([[1, 2],
+                       [3, 4]])
+test_array2 = np.array([[4, 3],
+                       [2, 1]])
+print(test_array1 @ test_array2)
+"""
+
+"""
+Important to note data types when using operators between arrays, the data type must be valid as they are not 
+automatically converted
+"""
+
+"""
+When you use operators between two valid types, the resulting array will be the type that is more precise
+"""
+
+"""
+There are multiple functions of arrays that you can call to analyze the information in them, some examples below
+test_array = np.arange(4).reshape(2, 2)
+print(test_array.sum())
+print(test_array.max())
+print(test_array.min())
+"""
+
+"""
+You can also specify the axis to use when calling those functions
+test_array = np.arange(12).reshape(2, 2, 3)
+print(test_array)
+print(test_array.sum(axis=0))
+print(test_array.sum(axis=1))
+print(test_array.sum(axis=2))
+"""
+
+"""
+You can also use normal mathematical functions with numpy array
+test_array = np.arange(5)
+print(np.exp(test_array))
+test_array2 = np.arange(5, 10)
+print(np.add(test_array, test_array2))
+print(np.sqrt(test_array))
+"""
+
+"""
+Indexing and slicing with arrays
+For each axis in the array that you are looking through, the bracket index [] has one more value
+for example, a 1d array would just be [value], a 2d array would be [value, value], 3d would be [value, value, value]
+These values can be one of two things: either an int, such as with a 3d array using [1, 1, 1]
+                                       Or it can be a range, which would mean all those values within that range
+                                       For example, with a 3d array you could do [0:5, 1, :]
+test_array = np.arange(10).reshape(2, 5)
+print(test_array)
+print(test_array[(0, 1)])
+print(test_array[:, 1])
+"""
+test_array = np.arange(27).reshape(3, 3, 3)
+print(test_array)
+print(test_array[0, 0, 0])
+print(test_array[:, 1, 0])
+print(test_array[:, 1:2, :])
+
